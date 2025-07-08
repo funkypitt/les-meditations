ac289579ae8edb85fef4c989b55cf92846485e67
Dmitriy Karpov <dmtkpv@gmail.com>
2025-07-08 18:58:53 +0300

    Revert "test database sync"
    This reverts commit 1bf2732bf7f2ca8dfd0407cf1986fb114a4b60db.

--- database/categories/bodyscan.js
+++ database/categories/bodyscan.js
@@ -1,5 +1,5 @@
 export default {
-  name: 'Bodyscan 2',
+  name: 'Bodyscan',
   slug: 'bodyscan',
   description: 'Le bodyscan est une méditation pratiquée habituellement en position couchée, qui consiste à prendre conscience une par une des différentes parties du corps.',
   recordings: [
@@ -30,11 +30,6 @@ export default {
     },
     {
       name: 'bodyscan, 15\' (12/15)',
-      description: '',
-      url: 'https://www.enpleineconscience.ch/fichiers/2015-12-16-mini_bodyscan.mp3'
-    },
-    {
-      name: 'test',
       description: '',
       url: 'https://www.enpleineconscience.ch/fichiers/2015-12-16-mini_bodyscan.mp3'
     }
