802daf7a6d996e771104cd268e53c9bf620f2f1c
Dmitriy Karpov <dmtkpv@gmail.com>
2025-07-08 12:40:08 +0300

    404 page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,21 +1,27 @@
-import Home from '#src/routes/r-home.vue';
-import Category from '#src/routes/r-category.vue';
-import Downloads from '#src/routes/r-downloads.vue';
+import RouteHome from '#src/routes/r-home.vue';
+import RouteCategory from '#src/routes/r-category.vue';
+import RouteDownloads from '#src/routes/r-downloads.vue';
+import Route404 from '#src/routes/r-404.vue';
 
 export default [
     {
         path: '/',
         name: 'home',
-        component: Home
+        component: RouteHome
     },
     {
         path: '/downloads',
         name: 'downloads',
-        component: Downloads
+        component: RouteDownloads
     },
     {
         path: '/category/:slug',
         name: 'category',
-        component: Category
-    }
+        component: RouteCategory
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404',
+        component: Route404
+    },
 ]
